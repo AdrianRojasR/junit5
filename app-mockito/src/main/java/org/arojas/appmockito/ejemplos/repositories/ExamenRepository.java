2165658a4f6b359e3f23cd5277ad1ddd972e3dbe
AdrianRojasR <36653926+AdrianRojasR@users.noreply.github.com>
2023-02-19 21:38:44 -0500

    tutorial junit5

---
+++ app-mockito/src/main/java/org/arojas/appmockito/ejemplos/repositories/ExamenRepository.java
@@ -0,0 +1,10 @@
+package org.arojas.appmockito.ejemplos.repositories;
+
+import org.arojas.appmockito.ejemplos.models.Examen;
+
+import java.util.List;
+
+public interface ExamenRepository {
+    Examen guardar(Examen examen);
+    List<Examen> findAll();
+}
